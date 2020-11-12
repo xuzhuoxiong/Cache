7a4bd4057bdcd502f34f1ddade289830befd55a5
xuzhuoxiong <504408157@qq.com>
2020-11-13 00:13:11 +0800

    工厂夹杂接口限制类

---
+++ Lib/Cache/Cache.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xu
+ * Date: 2020/11/12
+ * Time: 22:57
+ */
+
+namespace Lib\Cache;
+
+
+class Cache
+{
+
+}
---
+++ Lib/Cache/Interfaces/CacheInterfaces.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: xu
+ * Date: 2020/11/12
+ * Time: 23:51
+ */
+
+namespace Lib\Cache\Interfaces;
+
+
+class CacheInterfaces
+{
+
+}
